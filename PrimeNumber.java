6b0d73484358a8f5f731cbee25e6ba8e90e4a251
scalersanthoshm <127006901+scalersanthoshm@users.noreply.github.com>
2023-03-05 12:06:22 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,13 @@
+package com.scaler.probsolving;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int input = 5;
+	}
+	
+	public static int isPrime(int input) {
+		return 1;
+	}
+
+}
