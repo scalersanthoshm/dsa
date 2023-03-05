17a07b28aa44b2c6e273d9ab5b427abc0fe251b3
scalersanthoshm <mail4santhoshm@gmail.com>
2023-03-05 12:25:58 +0530

    test

--- src/main/java/com/scaler/arrays/PrimeNumber.java
+++ src/main/java/com/scaler/arrays/PrimeNumber.java
@@ -1,1 +1,14 @@
+package com.scaler.probsolving;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int input = 5;
+	}
+	
+	public static int isPrime(int input) {
+		return 1;
+	}
+
+}
 
